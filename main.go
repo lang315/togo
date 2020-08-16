ca61a41995115ae5c76f6646b8f14e2ee031f338
Trịnh Công Huy <30039912+lang315@users.noreply.github.com>
2020-08-16 21:23:13 +0700

    Update main.go

--- main.go
+++ main.go
@@ -2,14 +2,12 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"github.com/go-pg/pg/v10"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	"github.com/lang315/togo/internal/bolt"
 	"github.com/lang315/togo/internal/storages"
 	"github.com/lang315/togo/internal/transport"
-	"time"
 )
 
 func main() {
@@ -51,44 +49,4 @@ func main() {
 	}
 
 	app.Logger.Fatal(app.Start(":1323"))
-}
-
-func testDB() {
-	db := pg.Connect(&pg.Options{
-		Addr:     ":5432",
-		User:     "postgres",
-		Password: "Conghuy.315@",
-		Database: "togo",
-	})
-
-	if err := db.Ping(context.TODO()); err != nil {
-		panic(err)
-	}
-
-	fmt.Println("DONE")
-
-	data := storages.Data{Db: db}
-	data.Migrate()
-
-	fmt.Println(data.ValidateUser("123"))
-	user := &storages.User{
-		ID: "123",
-		// Password: "Zxcv1234",
-		// MaxToDo:  5,
-	}
-
-	fmt.Println(data.Db.Model(&storages.User{}).Where("id=?", user.ID).Delete())
-}
-
-func testReset() {
-	deadline, err := time.Parse("15:04", "23:59")
-	if err != nil {
-		panic(err)
-	}
-
-	for {
-		if time.Now().After(deadline) {
-
-		}
-	}
 }
